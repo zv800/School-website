8332daa0fa3ab5c20623c6d9b6c440e9f9f68bfb
zv800 <58888808+zv800@users.noreply.github.com>
2020-01-16 18:06:58 -0600

    Add files via upload

---
+++ s.js
@@ -0,0 +1,4 @@
+I want to tell you a little bit about myself
+
+this website is a personal little website that I made at school and at home I actually have ADHD learning disabilities and unspecified trauma disorder and anxiety. I used to be interested in electronics I still am but not as much anymore I love coding,the internet,computers,and really any kind of tech.I have to take 9 pills in the morning,a half a pill at lunch time,and 5 and a half pills at dinner I have to have two baby monitors because of my anxiety
+
